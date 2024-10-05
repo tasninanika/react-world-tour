abad536a61993d0dbadab8602d522165007e5da7
anikatasnin <jarintasnin09@gmail.com>
2024-10-05 23:56:49 +0000

    updated

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import country from "../Country/country";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -7,12 +8,15 @@ const Countries = () => {
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then( res => res.json())
-        .then(data => console.log(data));
+        .then(data => setCountries(data));
     }, [])
 
     return (
         <div>
-            <h3>Countries</h3>
+            <h3>Countries: {countries.length}</h3>
+            {
+                countries.map(country => <country></country>)
+            }
         </div>
     );
 };
