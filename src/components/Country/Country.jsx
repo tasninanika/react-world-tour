242c594365fd6f8c5248748c34f6779e866e7258
anikatasnin <jarintasnin09@gmail.com>
2024-10-20 11:37:13 +0000

    updated

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,10 @@
 import './Country.css';
 const Country = ({country}) => {
+    console.log(country);
     const {name, flags} = country;
     return (
         <div className='country'>
-            <h3>Name: {name.common}</h3>
+            <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
         </div>
     );
