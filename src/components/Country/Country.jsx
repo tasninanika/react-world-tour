e705e72c423182eb6dd6e463613e3180c77ee522
anikatasnin <jarintasnin09@gmail.com>
2024-10-23 18:31:01 +0000

    updated

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country, handleVisitedCountry}) => {
+const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
@@ -20,6 +20,7 @@ const Country = ({country, handleVisitedCountry}) => {
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
             <button onClick={() =>handleVisitedCountry(country)}>Mark visited</button>
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
             <br></br>
             <button onClick={handleVisited}>{visited? 'Visited' : 'Going'}</button>
             {visited && 'I have visited this country.'}
